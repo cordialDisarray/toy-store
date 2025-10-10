b49d49e0cdc15fbe4e24655ba4dd57a7b3a2ec75
cordialDisarray <166568225+cordialDisarray@users.noreply.github.com>
2025-10-10 22:44:23 +0200

    feat: add basic HTML layout for Home page
    -Implemented basic responsive layout using Flexbox
    -Very minimal styling for clarity
    -Added Angular Material
    -Added routing and navbar in app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Shop } from './shop/shop';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Account } from './account/account';
+import { Cart } from './cart/cart';
+import { Toy } from './toy/toy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'home', component: Home},
+    {path: 'shop', component: Shop},
+    {path: 'login', component: Login},
+    {path: 'signup', component: Signup},
+    {path: 'account', component: Account},
+    {path: 'cart', component: Cart},
+    {path: 'toy', component: Toy},
+];
