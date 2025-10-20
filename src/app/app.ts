8c9da54f32ed161fc3d9eed1ff25398645d093ca
cordialDisarray <166568225+cordialDisarray@users.noreply.github.com>
2025-10-20 19:27:07 +0200

    refactor: change Angular Material elements for basic HTML and CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,7 +13,8 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     MatIconModule
   ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class App {
   protected readonly title = signal('toy-store');
